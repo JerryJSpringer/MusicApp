a5b31cf597d991622f223879f5c0ec1284d49d03
JerryJSpringer <JerryJSpringer@gmail.com>
2020-11-21 10:05:17 -0800

    New fragment for songs.

---
+++ app/src/main/java/com/jerryjspringer/musicapp/ui/song/SongFragment.java
@@ -0,0 +1,34 @@
+package com.jerryjspringer.musicapp.ui.song;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.jerryjspringer.musicapp.MainActivity;
+import com.jerryjspringer.musicapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SongFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_song, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ((MainActivity) getActivity()).playAudio();
+    }
+}
