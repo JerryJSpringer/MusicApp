07770a8305dbfe6e05cf5fa4befeeacbe4e9be19
JerryJSpringer <JerryJSpringer@gmail.com>
2020-11-21 05:44:55 -0800

    Now has a list view that shows artists.

--- app/src/main/java/com/jerryjspringer/musicapp/ui/library/LibraryFragment.java
+++ app/src/main/java/com/jerryjspringer/musicapp/ui/library/LibraryFragment.java
@@ -3,7 +3,9 @@ package com.jerryjspringer.musicapp.ui.library;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
 
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -11,8 +13,12 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
+import com.jerryjspringer.musicapp.MainActivity;
 import com.jerryjspringer.musicapp.R;
+import com.jerryjspringer.musicapp.audio.AudioUtil;
+import com.jerryjspringer.musicapp.databinding.FragmentLibraryBinding;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,6 +30,18 @@ public class LibraryFragment extends Fragment {
                              Bundle savedInstanceState) {
         setHasOptionsMenu(true);
         return inflater.inflate(R.layout.fragment_library, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        FragmentLibraryBinding binding = FragmentLibraryBinding.bind(view);
+
+        binding.listRoot.setAdapter(
+                new ArtistRecyclerViewAdapter(AudioUtil.getArtists(getContext())));
+        binding.listRoot.addItemDecoration(
+                new DividerItemDecoration(getActivity(), LinearLayout.VERTICAL));
     }
 
     @Override
